576cd7c3a6858f5d819cf5a888d1b40589c14722
a-nima <dannievasileva@gmail.com>
2020-12-08 15:40:48 +0200

    Added second Star solution for Day 7

--- solutions/day7.mjs
+++ solutions/day7.mjs
@@ -1,38 +1,46 @@
 export function bags(input) {
     const targetColor = 'shiny gold';
-    let containingBagsRules = [];
+    let innerBagsRules = []; 
     let bags = [];
+    let innerBagsCount = 0;
 
-    let rules = input.split(/\n/);
-
-    for (const bagRules of rules) {
+    for (const bagRules of input.split(/\n/)) {
         let pattern = /((\d+)?\s?(\w+\s\w+))\sbags?/g;
         let matchGroup = pattern.exec(bagRules);
         let parentBag = matchGroup[3];
-        containingBagsRules[parentBag] = [];
+        innerBagsRules[parentBag] = [];
+
         while (matchGroup = pattern.exec(bagRules)) {
             if (matchGroup[3] != 'no other') {
                 let count = Number(matchGroup[2]);
-                containingBagsRules[parentBag].push(matchGroup[3])
+                innerBagsRules[parentBag][matchGroup[3]] = count;
             }
         }
     }
 
-    console.log(containingBagsRules);
-
     const findContainingBags = (currentTargetColor) => {
-        for (const key of Object.keys(containingBagsRules)) {
-            if (containingBagsRules[key].indexOf(currentTargetColor) > -1 && bags.indexOf(key) == -1) {
+        for (const key of Object.keys(innerBagsRules)) {
+            if (Object.keys(innerBagsRules[key]).indexOf(currentTargetColor) > -1 && bags.indexOf(key) == -1) {
                 bags.push(key);
                 findContainingBags(key);
             }
         }
     }
+
     findContainingBags(targetColor);
     console.log(bags.length);
 
     const countBagsInShinyGoldBag = (currentTargetColor) => {
-        
+        let innerBags = Object.keys(innerBagsRules[currentTargetColor]);
+        for (const innerBag of innerBags) {
+            const count = innerBagsRules[currentTargetColor][innerBag];
+            for (let index = 0; index < count; index++) {
+                innerBagsCount++;
+                countBagsInShinyGoldBag(innerBag);
+            }
+        }
     }
 
+    countBagsInShinyGoldBag(targetColor);
+    console.log(innerBagsCount);
 }
